3f173b53841d72c9cfdb63592c7dfb771d1b347a
Christopher Drew <github@rotarydial.com>
2017-10-21 00:17:44 -0400

    Script to start mongo after moving to linked dir

---
+++ mongostart.sh
@@ -0,0 +1,3 @@
+# After moving the data directory to /media/media_drives/Media/prog/data/mongodb
+# mongo doesn't want to start as a service. Have to run:
+sudo /usr/bin/mongod --config /etc/mongodb.conf
