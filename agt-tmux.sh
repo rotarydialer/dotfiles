2b026ce9b3c992083b4c6b2b77544d5887fbe05c
Christopher Drew <github@rotarydial.com>
2017-12-04 01:11:50 -0500

    tmux script to launch mongo and node
     Changes to be committed:
    	new file:   agt-tmux.sh

---
+++ agt-tmux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SESSION="agt"
+
+tmux -2 new-session -d -s $SESSION
+
+tmux new-window -t $SESSION:1 -n 'ag-trk'
+tmux split-window -v
+tmux resize-pane -U 25
+tmux select-pane -t 0
+tmux send-keys "~/mongostart.sh" C-m
+tmux select-pane -t 1
+tmux send-keys "sleep 5 && ~/agt.sh" C-m
+
+# Set default window
+tmux select-window -t $SESSION:1
+tmux select-pane -t 0
+
+# Attach to session
+tmux -2 attach-session -t $SESSION
